11f4eaa4b81e534502d9b042bb7aafb36f03eaa7
jeongsaeyeong <ooooo0516@naver.com>
2024-06-17 20:25:26 +0900

    20240617

---
+++ src/components/Main/Section03.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section03 = () => {
+  return (
+    <div>Section03</div>
+  )
+}
+
+export default Section03
---
+++ src/components/Main/Section04.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section04 = () => {
+  return (
+    <div>Section04</div>
+  )
+}
+
+export default Section04
